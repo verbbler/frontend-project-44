0184319f78af213f80fce3be5e3359e1b8393c87
verbbler <126270534+verbbler@users.noreply.github.com>
2024-09-06 15:00:10 +0300

    commit

---
+++ bin/brain-games.js
@@ -0,0 +1,6 @@
+// import game from "../src/cli";
+
+// game();
+// #!/usr/bin/env node.
+
+console.log("Welcome to the Brain Games!");
---
+++ src/cli.js
@@ -0,0 +1,10 @@
+// import readlineSync from "readline-sync";
+
+// const game = () => {
+//   console.log("Welcome to the Brain Games!");
+//   // console.log('May I have your name?')
+//   const name = readlineSync.question("Your answer: ");
+//   console.log(name);
+// };
+
+// export default game;
